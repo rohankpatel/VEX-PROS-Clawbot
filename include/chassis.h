bfa0ddf0b0486c27dc4ef67fdfcfd7c63f3ba6a6
Rohan Patel <rohanpatel@rohans-mbp.attlocal.net>
2018-01-18 06:35:09 -0600

    First commit

---
+++ include/chassis.h
@@ -0,0 +1,7 @@
+#ifndef _CHASSIS_H_
+#define _CHASSIS_H_
+
+//Sets speed of left and right wheels
+void chassisSet(int left, int right);
+
+#endif // _CHASSIS_H_
---
+++ src/claw.c
@@ -0,0 +1,6 @@
+#include "main.h"
+#include "claw.h"
+
+void clawSet(int speed) {
+  motorSet(9, -speed);
+}
